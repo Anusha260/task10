dbec631bf97aa75e38ea3ebbf50556f58da8d231
Your Name <you@example.com>
2021-12-16 01:40:21 +0530

    changes

--- mark10.js
+++ mark10.js
@@ -1,4 +1,4 @@
-const billAmount = docuconst billAmount = document.querySelector("#bill-amount");
+const billAmount = document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("#cash-given");
 const checkButton = document.querySelector("#check-button");
 const message = document.querySelector("#error-message");
